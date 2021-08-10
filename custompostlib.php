46a99358891a6ca22485a0df48f06bfabdc3d199
TadahiroYamamura <mt.village12@gmail.com>
2021-08-10 20:33:19 +0900

    プラグインの最小構成を作成

---
+++ custompostlib.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: WP Custom Post Library
+ * Plugin URI:  https://github.com/TadahiroYamamura/wp-custom-post-lib
+ * Description: Class library for creating your custom post.
+ * Version:     0.1.0
+ * Author:      Tadahiro Yamamura
+ * Author URI:  https://github.com/TadahiroYamamura
+ * License:     MIT
+ */
