973954672656ec2c43b0dad1de42c4185d5afc88
TadahiroYamamura <mt.village12@gmail.com>
2021-08-11 11:43:48 +0900

    「複数のメタ情報」はクラスではなく属性で表現する

--- classes/PostMetaData.php
+++ classes/PostMetaData.php
@@ -6,18 +6,35 @@ require_once(__DIR__.'/AbstractData.php');
 // postmetaに値をひとつ保存するデータクラス
 class PostMetaData extends \WpCustomPostLib\AbstractData {
   protected $accept_html = false;
+  protected $multi = false;
   protected $default_value = null;
 
   public function do_save(int $post_id, \WP_Post $post, bool $update) {
     if (!isset($_POST[$this->name])) return;
-    if (!is_null($this->default_value) && empty($_POST[$this->name])) {
-      $_POST[$this->name] = is_callable($this->default_value) ? call_user_func($this->default_value, $post_id, $post, $update) : $this->default_value;
+
+    if ($this->multi) {
+      $this->set_default_value($post_id, $post, $update);
+      delete_post_meta($post_id, $this->name);
+      foreach ($_POST[$this->name] as $v) {
+        add_post_meta($post_id, $this->name, $this->sanitize($v));
+      }
+    } else {
+      $this->set_default_value($post_id, $post, $update);
+      update_post_meta($post_id, $this->name, $this->sanitize($_POST[$this->name]));
     }
-    update_post_meta($post_id, $this->name, $this->sanitize($_POST[$this->name]));
   }
 
   public function do_extract(\WP_Post $post) {
-    return $this->contaminate(get_post_meta($post->ID, $this->name, true));
+    if ($this->multi) {
+      return array_map([$this, 'contaminate'], get_post_meta($post->ID, $this->name, false));
+    } else {
+      return $this->contaminate(get_post_meta($post->ID, $this->name, true));
+    }
+  }
+
+  public function multiple(bool $flg=true): self {
+    $this->multi = $flg;
+    return $this;
   }
 
   public function accept_html(bool $flag=true): self {
@@ -36,5 +53,15 @@ class PostMetaData extends \WpCustomPostLib\AbstractData {
 
   protected function contaminate($data) {
     return $this->accept_html ? esc_textarea($data) : $data;
+  }
+
+  private function set_default_value(int $post_id, \WP_Post $post, bool $update) {
+    if (is_null($this->default_value)) return;
+    if (!empty($_POST[$this->name])) return;
+    if (is_callable($this->default_value)) {
+      $_POST[$this->name] = call_user_func($this->default_value, $post_id, $post, $update);
+    } else {
+      $_POST[$this->name] = $this->multi ? [$this->default_value] : $this->default_value;
+    }
   }
 }
