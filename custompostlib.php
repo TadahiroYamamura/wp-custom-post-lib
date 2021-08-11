690df3ded56925472859a70316a9049bda50ea15
TadahiroYamamura <mt.village12@gmail.com>
2021-08-11 10:16:05 +0900

    require文を追加

--- custompostlib.php
+++ custompostlib.php
@@ -8,3 +8,15 @@
  * Author URI:  https://github.com/TadahiroYamamura
  * License:     MIT
  */
+
+require_once(__DIR__.'/classes/AbstractData.php');
+require_once(__DIR__.'/classes/AdminTable.php');
+require_once(__DIR__.'/classes/CustomPost.php');
+require_once(__DIR__.'/classes/CustomTaxonomy.php');
+require_once(__DIR__.'/classes/PostContentData.php');
+require_once(__DIR__.'/classes/PostImageData.php');
+require_once(__DIR__.'/classes/PostMetaData.php');
+require_once(__DIR__.'/classes/PostMetaDataMultiple.php');
+require_once(__DIR__.'/classes/PostTermData.php');
+require_once(__DIR__.'/classes/PostThumbnailData.php');
+require_once(__DIR__.'/classes/Renderer.php');
