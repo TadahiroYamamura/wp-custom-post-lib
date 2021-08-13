dd3344608b20544267743acb876a0204287d3f51
TadahiroYamamura <mt.village12@gmail.com>
2021-08-13 21:24:27 +0900

    各関数にオプションを渡せるように変更

--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,13 @@
 <?php
 namespace WpCustomPostLib;
 
-function admin_input($name, $label, $value, $type="text") {
+function admin_input($name, $label, $value, $options=[]) {
+  $options = array_merge([
+    'type' => 'text',
+    'class' => '',
+  ], $options);
 ?>
-  <label class="admin-input-component <?= $type ?>">
+  <label class="admin-input-component <?= $options['type'] ?> <?= $options['class'] ?>">
     <input type="<?= $type ?>" class="component" name="<?= $name ?>" placeholder=" " value="<?= $value ?>">
     <span class="label"><?= $label ?></span>
   </label>
@@ -12,30 +16,39 @@ function admin_input($name, $label, $value, $type="text") {
 
 
 
-function admin_textarea($name, $label, $value) {
+function admin_textarea($name, $label, $value, $options=[]) {
+  $options = array_merge([
+    'class' => '',
+  ], $options);
 ?>
-  <label class="admin-input-component textarea">
+  <label class="admin-input-component textarea <?= $options['class'] ?>">
     <textarea class="component" name="<?= $name ?>" placeholder=" "><?= $value ?></textarea>
     <span class="label"><?= $label ?></span>
   </label>
 <?php
 }
 
-function admin_checkbox($name, $label, $value, $checked) {
+function admin_checkbox($name, $label, $value, $checked, $options=[]) {
+  $options = array_merge([
+    'class' => '',
+  ], $options);
 ?>
-  <label class="admin-input-component checkbox">
+  <label class="admin-input-component checkbox <?= $options['class'] ?>">
     <input type="checkbox" class="component" name="<?= $name ?>" value="<?= $value ?>" <?= checked($checked) ?>>
     <span class="label"><?= $label ?></span>
   </label>
 <?php
 }
 
-function admin_select($name, $label, $options, $current_value) {
+function admin_select($name, $label, $items, $current_value, $options=[]) {
+  $options = array_merge([
+    'class' => '',
+  ], $options);
 ?>
-  <label class="admin-input-component select">
+  <label class="admin-input-component select <?= $options['class'] ?>">
     <select class="component" name="<?= $name ?>">
       <option value="" hidden></option>
-      <?php foreach ($options as $value => $text): ?>
+      <?php foreach ($items as $value => $text): ?>
         <option value="<?= $value?>" <?= selected($value === $current_value) ?>><?= $text ?></option>
       <?php endforeach; ?>
     </select>
